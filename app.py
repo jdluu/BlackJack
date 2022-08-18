c4888bcbec033304e2d919caeb25b164c024f197
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2022-08-17 21:57:04 -0700

    Renamed

--- app.py
+++ app.py
@@ -1,29 +1,28 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask, render_template
+from flask import Flask, request, render_template
  
 # Flask constructor takes the name of
 # current module (__name__) as argument.
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="")
  
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
-@app.route('/')
-@app.route('/home/')
-def home():
-    return render_template('home.html')
+@app.route('/home')
+def get_homepage():
+    return render_template("home.html")
 
 @app.route('/game/')
-def game():
-    return render_template('game.html')
+def get_blackjack():
+    return render_template('blackjack.html')
 
 @app.route('/rules/')
-def rules():
+def get_rules():
     return render_template('rules.html')
 
 @app.route('/stats/')
-def stats():
+def get_stats():
     return render_template('stats.html')
  
 # main driver function
