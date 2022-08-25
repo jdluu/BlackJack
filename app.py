d13e519f3aff8cf68b67346bad57bb909dfe20e3
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2022-08-25 16:07:40 -0700

    edited app to include blackjack rendering

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
 from flask import Flask, request, render_template
+
+import BlackJack
  
 # Flask constructor takes the name of
 # current module (__name__) as argument.
@@ -9,25 +11,38 @@ app = Flask(__name__, static_url_path="")
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
-@app.route('/home')
+
+@app.route('/')
 def get_homepage():
     return render_template("home.html")
 
-@app.route('/game/')
-def get_blackjack():
-    return render_template('blackjack.html')
+@app.route('/blackjack/')
+def getBlackjack():
+    BlackJack.startHand()
+    return render_template('blackjack.html', handResults = BlackJack.handResults(), playerCards = BlackJack.playerCards(), dealerCards = BlackJack.dealerCards())
+
+@app.post("/blackjack/")
+def postBlackJack():
+    submitButton = request.form.get("submitButton")
+
+    if BlackJack.isPlayerTurn():
+        if submitButton.upper() == "H":
+            BlackJack.playerHit()
+        else:
+            BlackJack.dealerPlay()
+    
+    return render_template('blackjack.html', handResults = BlackJack.handResults(), playerCards = BlackJack.playerCards(), dealerCards = BlackJack.dealerCards())
 
 @app.route('/rules/')
-def get_rules():
+def getRules():
     return render_template('rules.html')
 
 @app.route('/stats/')
-def get_stats():
+def getStats():
     return render_template('stats.html')
- 
+
 # main driver function
-if __name__ == '__main__':
- 
+if __name__ == "__main__":
     # run() method of Flask class runs the application
     # on the local development server.
-    app.run()
+     app.run(debug=True ,port=5000,use_reloader=False)
